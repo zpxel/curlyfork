0e0511ce35489dd56482ffe04ff2f965322fe018
zpxel <zpixeljy@gmail.com>
2024-12-22 03:53:34 +0800

    Update devian.sh
    Extended bash

--- devian.sh
+++ devian.sh
@@ -7,6 +7,11 @@ pkg install git
 pkg install zip
 termux-setup-storage
 pkg install p7zip
+cd
+cd curlyfork
+ls
 mv funny.mp4 /data/data/com.termux/files/home/storage/downloads
+cd
+cd curlyfork
 chmod +x curlyfork.py
 python curlyfork.py
